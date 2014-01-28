dad92d92f635ca612025f4d77772e416107d76f1
Astr0surf3r <astr0surf3r@gmail.com>
2014-01-28 20:31:02 +0100

    add scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tjbit::Application.config.secret_token = '67cf2a33fe4fb0e71dc16cb44c2cc8e80869ed8b882b14302549c817c1fea0e21f13f3141a078437f146c95e6b7485ee354043904398946989d80e17ff1e6d81'
